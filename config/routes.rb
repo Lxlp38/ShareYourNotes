72ed705851064a8d63f88602d9528f136937ffec
Lxlp38 <baravitto2@gmail.com>
2024-06-06 23:24:39 +0200

    added logout button

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+
   devise_for :users, controllers: {
       sessions: 'users/sessions',
       omniauth_callbacks: 'users/omniauth_callbacks'
     }
+
+    devise_scope :user do
+      get '/users/sign_out', to: 'users/sessions#destroy'
+    end
+    
   resources :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'users#index'
 
