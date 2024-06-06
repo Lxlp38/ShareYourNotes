bcd4bc1c873c8002c6994f109bd99ec9f183e224
Lxlp38 <baravitto2@gmail.com>
2024-06-06 12:34:14 +0200

    created views and corrected devise syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+      sessions: 'users/sessions'
+    }
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'users#index'
