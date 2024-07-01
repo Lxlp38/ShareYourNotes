51049febc7ed8c2fbb74a9533733104d1c1f8d4e
Hepe2002 <gdellaratta02@gmail.com>
2024-07-01 12:36:19 +0200

    un po' di registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,33 +1,33 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+   before_action :configure_sign_up_params, only: [:create]
+   before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+     super
+  end
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+  #POST /resource
+  def create
+    super
+  end
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
+   #GET /resource/edit
+   def edit
+     super
+   end
 
-  # PUT /resource
-  # def update
-  #   super
-  # end
+   #PUT /resource
+   def update
+     super
+   end
 
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
+   #DELETE /resource
+   def destroy
+     super
+   end
 
   # GET /resource/cancel
   # Forces the session data which is usually expired after sign
@@ -38,22 +38,22 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :current_password])
+  end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+     super(resource)
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
