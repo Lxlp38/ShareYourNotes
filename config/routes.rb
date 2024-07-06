f4fa48b192a853bd69aef3215eb645b0d20f255d
1986191 <mariut.1986191@studenti.uniroma1.it>
2024-07-06 22:44:27 +0200

    Added about us page

--- config/routes.rb
+++ config/routes.rb
@@ -31,6 +31,7 @@ Rails.application.routes.draw do
   resources :users
 
   match '/frequently_asked' => "application#frequently_asked" , as: 'faq', via: [:get]
+  match '/about_us' => "application#about_us" , as: 'about', via: [:get]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'notes#index'
