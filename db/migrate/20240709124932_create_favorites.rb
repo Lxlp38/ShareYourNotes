4c8ee33eeae687bf9a56861e2d7fed54d6e5609f
1986191 <mariut.1986191@studenti.uniroma1.it>
2024-07-09 17:25:13 +0200

    Corrected migration

--- db/migrate/20240709124932_create_favorites.rb
+++ db/migrate/20240709124932_create_favorites.rb
@@ -3,7 +3,7 @@ class CreateFavorites < ActiveRecord::Migration[6.1]
     create_table :favorites do |t|
       t.references :user, null: false, foreign_key: true
       t.references :note, null: false, foreign_key: true
-      t.boolean :favorite, default true
+      t.boolean :favorite, default: true
 
       t.timestamps
     end
