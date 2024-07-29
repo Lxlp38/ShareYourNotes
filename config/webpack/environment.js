6d6f78681dfe5e0bab4a6c8dca17d345f6459022
giorgiokr <giorgio.apa96kr@gmail.com>
2024-07-29 15:43:18 +0200

    fix javascript

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,38 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    process: 'process/browser',
+    Buffer: ['buffer', 'Buffer']
+  })
+)
+
+environment.config.merge({
+  resolve: {
+    fallback: {
+      buffer: require.resolve('buffer/'),
+      process: require.resolve('process/browser'),
+      dgram: false,
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false
+    }
+  },
+  node: {
+    global: true,
+    __filename: true,
+    __dirname: true
+  }
+})
+
+environment.config.delete('node.dgram')
+environment.config.delete('node.fs')
+environment.config.delete('node.net')
+environment.config.delete('node.tls')
+environment.config.delete('node.child_process')
+
 
 module.exports = environment
