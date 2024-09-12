3f44390f88f610c7b768a2e9d375430a9dbbc40c
giorgiokr <giorgio.apa96kr@gmail.com>
2024-09-12 15:37:22 +0200

    test di login
    Caricare il db per eseguire i test

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,18 +1,17 @@
 require 'rspec/expectations'
 
-
 Given("I am on the login page") do
-    visit new_user_session_path
-    # Assicurati che `new_user_session_path` sia il percorso corretto per la pagina di login
+  visit new_user_session_path  # Assicurati che `new_user_session_path` sia il percorso corretto per la pagina di login
 end
+
 When('I fill in the login {string} with {string}') do |field, value|
-    fill_in field, with: value
-  end
-  
-  When('I click the login {string} button') do |button|
-    click_button button
-  end
-  
-  Then (/^I should be on the notes page$/) do
-    expect(current_path).to eq(notes_path)
-  end
+  fill_in field, with: value
+end
+
+And('I click the login {string} button') do |button|
+  click_button button
+end
+
+Then('I should be on the notes page') do
+  expect(current_path).to eq("/")
+end
